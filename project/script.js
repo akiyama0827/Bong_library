69d35c31401229aaea967e9a80069d721888de94
akiyama0827__ <whwowns1021@gmail.com>
2023-06-07 17:56:10 +0900

    s

--- project/script.js
+++ project/script.js
@@ -1,7 +1,7 @@
 const SearchBtn = document.querySelector('#search-btn');
 
 function SearchpageRedirect() {
-    window.open('https://reading.cbe.go.kr/r/newReading/search/schoolSearchForm.jsp');
+    window.open('https://reading.cbe.go.kr/r/newReading/search/schoolSearchOnlyForm.jsp?schoolCode=30469&kind=1');
 }
 
 SearchBtn.addEventListener('click', SearchpageRedirect);
