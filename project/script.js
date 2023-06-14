1c8dfbf67f1adc08c9a8ef1ced180ef640724ef0
akiyama0827__ <whwowns1021@gmail.com>
2023-06-15 00:03:22 +0900

    fuck

--- project/script.js
+++ project/script.js
@@ -1,7 +1,18 @@
+const newbooksBtn = document.querySelector('#newbooks-btn');
+const hottestbooksBtn = document.querySelector('#hottestbooks-btn');
 const SearchBtn = document.querySelector('#search-btn');
+const boardBtn = document.querySelector('#board-btn');
 
-function SearchpageRedirect() {
+function NewbooksPageRedirect() {
+    window.open('https://reading.cbe.go.kr/r/newReading/search/schoolNewBookForm.jsp?schoolCode=30469&kind=1');
+}
+function HottestbooksPageRedirect() {
+    window.open('https://reading.cbe.go.kr/r/newReading/search/schoolBestBookForm.jsp?schoolCode=30469&kind=1');
+}
+function SearchPageRedirect() {
     window.open('https://reading.cbe.go.kr/r/newReading/search/schoolSearchOnlyForm.jsp?schoolCode=30469&kind=1');
 }
 
-SearchBtn.addEventListener('click', SearchpageRedirect);
+newbooksBtn.addEventListener('click', NewbooksPageRedirect);
+hottestbooksBtn.addEventListener('click', HottestbooksPageRedirect);
+SearchBtn.addEventListener('click', SearchPageRedirect);
